f05b927bdd986cbac5284f634bf7595f1b0447c4
Keklord228322 <zazizoid@gmail.com>
2021-03-20 15:19:57 +0300

    dobavil

---
+++ nemain.js
@@ -0,0 +1,9 @@
+const obj = {
+  hui: 229,
+  pizda: 228,
+  djigurda: 322,
+};
+
+console.log(Object.keys(obj).join(" sosed "));
+console.log("hui1pizda1dhigurda1".includes("1"));
+console.log(Object.values(obj));
