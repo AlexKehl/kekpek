7aff7d4876ae2d171410f5a455a594702a487020
Alex Kehl <alexander.kehl@apprologic.de>
2021-03-20 16:12:31 +0100

    Fix codi

--- graphParser.js
+++ graphParser.js
@@ -13,18 +13,20 @@ const MILIS_IN_WEEK =
   DAYS_IN_WEEK;
 const oneWeekAgoMilis = date - MILIS_IN_WEEK;
 const dataItemLastWeek = [];
-const lastWeekPrices = dataitem.prices.filter(isDataOfLastWeek);
 function isDataOfLastWeek(oneDataItem) {
   const todayMilis = date;
   const pastMilis = new Date(oneDataItem[0]).valueOf();
   return todayMilis - pastMilis < MILIS_IN_WEEK;
 }
+const lastWeekPrices = dataitem.prices.filter(isDataOfLastWeek);
 const price = 0;
-const avgPriceArrSum = lastWeekPrices
-  .map((lastWeekPrice) => lastWeekPrice[1] * parseInt(lastWeekPrice[2]))
-  .reduce(function (acc, currentvalue) {
-    return acc + currentvalue;
-  });
+const avgPriceArr = lastWeekPrices.map(
+  (lastWeekPrice) => lastWeekPrice[1] * parseInt(lastWeekPrice[2])
+);
+
+const avgPriceArrSum = avgPriceArr.reduce(function (acc, currentvalue) {
+  return acc + currentvalue;
+});
 const lastWeekPricesCount = lastWeekPrices.reduce(function (acc, currentvalue) {
   return acc + parseInt(currentvalue[2]);
 }, 0);
