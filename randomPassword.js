68d89ccff56d4cd2226d346f3f4e11ca420680d8
Keklord228322 <zazizoid@gmail.com>
2021-03-20 16:27:13 +0300

    vat_changed_on_let

--- randomPassword.js
+++ randomPassword.js
@@ -1,16 +1,16 @@
-var abcCAPS = "QWERTYUIOPLKJHGFDSAZXCVBNM",
+let abcCAPS = "QWERTYUIOPLKJHGFDSAZXCVBNM",
   wordCAPS = "";
 for (let i = 0; i < 2; i++) {
   wordCAPS += abcCAPS[Math.round(Math.random() * (abcCAPS.length - 1))];
 }
 
-var numbers = "0123456798",
+let numbers = "0123456798",
   wordnum = "";
 for (let i = 0; i < 2; i++) {
   wordnum += numbers[Math.round(Math.random() * (numbers.length - 1))];
 }
 
-var abcreg = "qwertyuioplkjhgfdsazxcvbnm",
+let abcreg = "qwertyuioplkjhgfdsazxcvbnm",
   wordreg = "";
 for (let i = 0; i < 7; i++) {
   wordreg += abcreg[Math.round(Math.random() * (abcreg.length - 1))];
